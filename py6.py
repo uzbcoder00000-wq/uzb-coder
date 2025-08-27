3b885a481738baf3297ef33d0e6b3189b41371b3
uzbcoder00000-wq <uzbcoder00000@gmail.com>
2025-08-27 15:06:54 +0500

    Dasturchi uchun boshlang'ich maslahatlardan biri: Funksiya e'lon qilish

---
+++ py6.py
@@ -0,0 +1,11 @@
+# 6. Funksiya e’lon qilish
+# Funksiya yordamida biror kodni qayta ishlatish mumkin
+
+def kvadrat(x):
+    """
+    Bu funksiya x sonining kvadratini hisoblaydi.
+    """
+    return x * x
+
+# Funksiyani chaqirish
+print(kvadrat(5))  # Natija: 25
