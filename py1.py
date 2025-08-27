34e282b2e8b70a9abc103aa24a3d253f27f8bb5e
uzbcoder00000-wq <uzbcoder00000@gmail.com>
2025-08-27 15:06:25 +0500

    Dasturchi uchun boshlang'ich maslahatlardan biri: docstring, comment

---
+++ py1.py
@@ -0,0 +1,9 @@
+# 1. Dastur haqida ma'lumot
+"""
+Bu fayl dastur haqida qisqacha ma'lumot beradi.
+Dastur nomi: Boshlang'ich misol
+Muallif: uzbcoder00000-wq
+Maqsad: Python kodida docstring va comment ishlatish
+"""
+# Misol uchun oddiy comment
+# Bu comment kodni tushunishga yordam beradi
