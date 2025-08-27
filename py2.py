a90d26a5cb8feedf63a4df3d1e95b1df1240d7b5
uzbcoder00000-wq <uzbcoder00000@gmail.com>
2025-08-27 15:06:29 +0500

    Dasturchi uchun boshlang'ich maslahatlardan biri: O'zgaruvchilar va print

---
+++ py2.py
@@ -0,0 +1,9 @@
+# 2. O‘zgaruvchilar va print
+# Python’da o‘zgaruvchi (variable) yaratish va ekranga chiqarish
+
+ism = "Alisher"  # O‘zgaruvchi yaratish
+yosh = 20        # Ikkinchi o‘zgaruvchi
+
+print(ism)       # Ekranga ismni chiqaradi
+print(yosh)      # Ekranga yoshni chiqaradi
+print(f"{ism} {yosh} yoshda.")  # Formatlangan chiqarish
