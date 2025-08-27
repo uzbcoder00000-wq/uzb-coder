8d310ff4bcd296b0d9b82aa6edae4b9e5e40e809
uzbcoder00000-wq <uzbcoder00000@gmail.com>
2025-08-27 15:06:41 +0500

    Dasturchi uchun boshlang'ich maslahatlardan biri: List (ro'yxatlar)

---
+++ py4.py
@@ -0,0 +1,8 @@
+# 4. Ro‘yxatlar (list)
+# Bir nechta qiymatlarni bitta ro‘yxat (list)da saqlash
+
+mevalar = ["olma", "banan", "uzum"]  # Ro‘yxat yaratish
+print("Mevalar ro‘yxati:", mevalar)
+
+# Ro‘yxatdan biror elementni olish
+print("Birinchi meva:", mevalar[0])
