2466113128caf24221a5e9e5c50a03f7ec22b2df
uzbcoder00000-wq <uzbcoder00000@gmail.com>
2025-08-27 15:06:35 +0500

    Dasturchi uchun boshlang'ich maslahatlardan biri: If-else shartlari

---
+++ py3.py
@@ -0,0 +1,9 @@
+# 3. If-else sharti
+# Shartli operatorlar yordamida turli vaziyatlarni tekshirish
+
+yosh = 16
+
+if yosh >= 18:
+    print("Siz kattasiz!")
+else:
+    print("Siz hali voyaga yetmagansiz.")
